0813e57342414aa4b38823b4da903e1ff9a3cd73
Sayan Mukherjee <sayan05mukherjee@gmail.com>
2025-08-09 21:15:03 +0530

    1st Solution

---
+++ 4.SayanMukharjee/Easy/twoSum.cpp
@@ -0,0 +1,16 @@
+//Two Sum Problem Solution
+//!https://leetcode.com/problems/two-sum/description/   [Question Link]
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j}; // return the indices
+                }
+            }
+        }
+        return {}; // if no pair found
+    }
+};
