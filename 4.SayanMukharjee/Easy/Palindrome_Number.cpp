d4e222b873788c9009bbeecb3bb41a31974eaa59
Sayan Mukherjee <sayan05mukherjee@gmail.com>
2025-08-11 21:53:01 +0530

    Done

---
+++ 4.SayanMukharjee/Easy/Palindrome_Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+    if(x<0) return false;
+    int org = x, rev = 0;
+    while(x>0){
+        int dig = x%10;
+        rev = rev*10+dig;
+        x = x/10;
+    }  
+    return org = rev;
+    }
+};
